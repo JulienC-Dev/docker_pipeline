d93d1b63647c109521394158c397f33970cd99b3
JulienC-Dev <julien.cormier@yahoo.fr>
2023-07-26 01:35:47 +0200

    update app for yaml deploy server

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello word, I am a Flask application!'
+    return 'Hello word, I am a live Flask application!'
 '''ATTENTION : LEAK DATA dans le cadre du projet
 user="root",
 password="p@ssw0rd1"
